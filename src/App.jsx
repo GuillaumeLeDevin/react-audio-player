2aed559377725f50dbd865ac1157c0f3c0cd9230
Guillaume Le Devin <guillaume.le.devin@gmail.com>
2025-07-29 23:47:56 +0100

    adjustment of the main title

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div className="min-h-screen bg-slate-800 pt-20 px-4">
         <div className="max-w-xl mx-auto">
           <Player />
-          <h1 className="text-slate-100 text-2xl">PlayerMania - Your songs</h1>
+          <h1 className="text-slate-100 text-2xl">PlayerMania - Your 5 songs</h1>
           <Playlist />
         </div>
       </div>
